ca3cf70711daf9c597d7180218067c24b2fe1779
jester69m <paschuknazar@ukr.net>
2022-06-02 22:59:04 +0300

    add small animation(need to draw some pictures)

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,7 +10,7 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; //16x16 pixels
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; // 48x48 size
+    public final int tileSize = originalTileSize * scale; // 48x48 size
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
@@ -19,6 +21,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     KeyHandler  keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     //set player position
     int playerX = 100;
@@ -72,19 +75,7 @@ public class GamePanel extends JPanel implements Runnable {
         }
     }
     public void update(){
-
-        if(keyH.upPressed == true){
-            playerY -= playerSpeed;
-        }
-        else if(keyH.downPressed == true){
-            playerY += playerSpeed;
-        }
-        else if(keyH.leftPressed == true){
-            playerX -= playerSpeed;
-        }
-        else if(keyH.rightPressed == true){
-            playerX += playerSpeed;
-        }
+        player.update();
 
 
 
@@ -95,9 +86,8 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.white);
+        player.draw(g2);
 
-        g2.fillRect(playerX,playerY,tileSize,tileSize);
 
         g2.dispose();
     }
