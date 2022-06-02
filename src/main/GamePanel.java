a826a2517ca60d7f498b1b85b52bcf79946e0a72
jester69m <paschuknazar@ukr.net>
2022-06-02 20:58:22 +0300

    editing GamePanel / Main

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     //Screen setting
     final int originalTileSize = 16; //16x16 pixels
     final int scale = 3;
@@ -14,6 +14,8 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    Thread gameThread;
+
     public GamePanel(){
 
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
@@ -21,5 +23,34 @@ public class GamePanel extends JPanel {
         this.setDoubleBuffered(true);
 
     }
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
 
+        while(gameThread != null){
+
+            update();
+
+            repaint();
+
+        }
+    }
+    public void update(){
+
+    }
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(100,100,tileSize,tileSize);
+
+        g2.dispose();
+    }
 }
